bd287fe972862060987e0c6aa634272bbded1fd3
Maverobot <quzhengrobot@gmail.com>
2019-09-15 20:50:47 +0200

    added rnn example

--- conan_cmake_example/src/mlpack_example.cpp
+++ conan_cmake_example/src/mlpack_example.cpp
@@ -1,14 +1,114 @@
-// Includes all relevant components of mlpack.
 #include <mlpack/core.hpp>
-// Convenience.
+#include <mlpack/methods/ann/ffn.hpp>
+#include <mlpack/methods/ann/rnn.hpp>
+
+void GenerateNoisySines(arma::cube &data, arma::mat &labels,
+                        const size_t num_points, const size_t num_sequences,
+                        const double noise = 0.3) {
+  arma::colvec x = arma::linspace<arma::colvec>(0, num_points - 1, num_points) /
+                   num_points * 20.0;
+  std::cout << "x = " << x << std::endl;
+  arma::colvec y1 = arma::sin(x + arma::as_scalar(arma::randu(1)) * 3.0);
+  arma::colvec y2 = arma::sin(x / 2.0 + arma::as_scalar(arma::randu(1)) * 3.0);
+  std::cout << "y1 = " << y1 << std::endl;
+  std::cout << "y2 = " << y2 << std::endl;
+
+  data = arma::zeros(1 /* single dimension */, num_sequences * 2, num_points);
+  labels = arma::zeros(2 /* 2 classes */, num_sequences * 2);
+
+  for (size_t seq = 0; seq < num_sequences; seq++) {
+    arma::vec sequence = arma::randu(num_points) * noise + y1 +
+                         arma::as_scalar(arma::randu(1) - 0.5) * noise;
+    for (size_t i = 0; i < num_points; ++i)
+      data(0, seq, i) = sequence[i];
+
+    labels(0, seq) = 1;
+
+    sequence = arma::randu(num_points) * noise + y2 +
+               arma::as_scalar(arma::randu(1) - 0.5) * noise;
+    for (size_t i = 0; i < num_points; ++i)
+      data(0, num_sequences + seq, i) = sequence[i];
+
+    labels(1, num_sequences + seq) = 1;
+  }
+  std::cout << data << std::endl;
+  std::cout << labels << std::endl;
+}
+
+using namespace mlpack::ann;
+using namespace mlpack::optimization;
 using namespace mlpack;
+
 int main(int argc, char *argv[]) {
+  size_t successes = 0;
+  const size_t rho = 10;
 
-  // Loads the data.
-  arma::Mat<double> data;
-  data::Load(argv[1], data, true, false);
+  arma::cube input;
+  arma::mat labelsTemp;
+  GenerateNoisySines(input, labelsTemp, rho, 6);
 
-  // Saves the first 10 lines
-  data::Save("data_head.csv", arma::mat(data.head_rows(10)), true);
-  return 0;
+  arma::cube labels = arma::zeros<arma::cube>(1, labelsTemp.n_cols, rho);
+  for (size_t i = 0; i < labelsTemp.n_cols; ++i) {
+    const int value =
+        arma::as_scalar(
+            arma::find(arma::max(labelsTemp.col(i)) == labelsTemp.col(i), 1)) +
+        1;
+    labels.tube(0, i).fill(value);
+  }
+
+  /**
+   * Construct a network with 1 input unit, 4 hidden units and 10 output
+   * units. The hidden layer is connected to itself. The network structure
+   * looks like:
+   *
+   *  Input         Hidden        Output
+   * Layer(1)      Layer(4)      Layer(10)
+   * +-----+       +-----+       +-----+
+   * |     |       |     |       |     |
+   * |     +------>|     +------>|     |
+   * |     |    ..>|     |       |     |
+   * +-----+    .  +--+--+       +-----+
+   *            .     .
+   *            .     .
+   *            .......
+   */
+  Add<> add(4);
+  Linear<> lookup(1, 4);
+  SigmoidLayer<> sigmoidLayer;
+  Linear<> linear(4, 4);
+  Recurrent<> *recurrent =
+      new Recurrent<>(add, lookup, linear, sigmoidLayer, rho);
+
+  RNN<> model(rho);
+  model.Add<IdentityLayer<>>();
+  model.Add(recurrent);
+  model.Add<Linear<>>(4, 10);
+  model.Add<LogSoftMax<>>();
+
+  StandardSGD opt(0.1, 1, 500 * input.n_cols, -100);
+  model.Train(input, labels, opt);
+
+  arma::cube prediction;
+  model.Predict(input, prediction);
+
+  size_t error = 0;
+  for (size_t i = 0; i < prediction.n_cols; ++i) {
+    const int predictionValue = arma::as_scalar(
+        arma::find(arma::max(prediction.slice(rho - 1).col(i)) ==
+                       prediction.slice(rho - 1).col(i),
+                   1) +
+        1);
+
+    const int targetValue =
+        arma::as_scalar(
+            arma::find(arma::max(labelsTemp.col(i)) == labelsTemp.col(i), 1)) +
+        1;
+
+    if (predictionValue == targetValue) {
+      error++;
+    }
+  }
+
+  double classificationError = 1 - double(error) / prediction.n_cols;
+  std::cout << "classification error: " << classificationError << std::endl;
 }
