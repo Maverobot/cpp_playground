641d45b527699e794b8491bd4ea76759185c7d5c
Maverobot <quzhengrobot@gmail.com>
2019-09-16 21:14:05 +0200

    the issue was that const conflicts with istringstream

--- csv_reader/src/csv_reader_ex.cpp
+++ csv_reader/src/csv_reader_ex.cpp
@@ -9,22 +9,19 @@
 
 #include <type_traits>
 
-struct LineParser {
-  LineParser(const std::string &csv_line) : line(csv_line) {}
-  template <typename T> T operator()(T &&) const {
-    std::istringstream iss(line);
-    T res = parse_token<T>(iss);
-    std::cout << "type: " << NAMEOF_TYPE(T) << ", res: " << res << std::endl;
-    return res;
-  }
-  std::string line;
+class LineParser {
+public:
+  LineParser(const std::string &csv_line) : iss(csv_line) {}
+  template <typename T> T operator()(T &&) const { return parse_token<T>(iss); }
+
+private:
+  mutable std::istringstream iss;
 };
 
 struct SensorData {
   SensorData() = default;
   SensorData(const std::string &csv_line) {
     LineParser line_parser(csv_line);
-    std::cout << "line: " << csv_line << std::endl;
     data = execute_all(line_parser, data);
   }
   std::tuple<std::string, int, int, double, double, double, double, double,
@@ -35,13 +32,8 @@ struct SensorData {
 int main(int argc, char *argv[]) {
 
   auto sensor_data = load_csv_file<SensorData>(argv[1]);
-  std::tuple<double, int> test{1.99, 2};
-  std::cout << test << std::endl;
-
-  std::get<9>(sensor_data[0].data) = 10;
   std::cout << sensor_data[0].data << std::endl;
-
-  std::cout << std::tuple<double, int>{1.1, 1} << std::endl;
+  std::cout << sensor_data[1].data << std::endl;
 
   return 0;
 }
