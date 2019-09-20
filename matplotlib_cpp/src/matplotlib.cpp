1c596b0b59ff2e4adddcbe5238336c51852aeb73
Maverobot <quzhengrobot@gmail.com>
2019-09-20 20:52:27 +0200

    tested matplotlib

--- matplotlib_cpp/src/matplotlib.cpp
+++ matplotlib_cpp/src/matplotlib.cpp
@@ -11,23 +11,27 @@ std::ostream &operator<<(std::ostream &o, const std::vector<T> &v) {
 }
 
 int main() {
-  // Prepare plotting the tangent.
-  plt::Plot plot("plot update");
 
+  plt::title("Canvas");
+  plt::axis("equal");
+
+  // Prepare plotting the tangent.
+  plt::Plot plot("linear increaser");
+  plt::legend();
+
+  const size_t kMaxVecSize = 100;
   std::vector<double> values;
   std::vector<double> x;
   double v = 0;
   while (true) {
-    std::cout << "step()" << std::endl;
     values.push_back(v);
     x.resize(values.size());
     std::generate(x.begin(), x.end(), [n = 0]() mutable { return n++; });
-    std::cout << x << std::endl;
-    std::cout << values << std::endl;
     plot.update(x, values);
-    plt::show(false);
     // Small pause so the viewer has a chance to enjoy the animation.
     plt::pause(0.1);
     v = v + 1;
+    plt::xlim(0, 1000);
+    plt::ylim(0, 1000);
   }
 }
