b9446fd1012c813f99a998b617c7b84af6b10df5
Maverobot <quzhengrobot@gmail.com>
2019-10-17 21:36:35 +0200

    added comments

--- sfinae_example/src/sfinae_example.cpp
+++ sfinae_example/src/sfinae_example.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <type_traits>
+#include <iostream>    // for cout and endl
+#include <type_traits> // for true_type and false_type
 
 // SFINAE out partial specialization
 template <typename...> using VoidT = void;
@@ -7,6 +7,17 @@ template <typename, typename = VoidT<>>
 struct isDefaultConstructableT : std::false_type {};
 template <typename T>
 struct isDefaultConstructableT<T, VoidT<decltype(T())>> : std::true_type {};
+
+/*
+  Be aware: with the following code SFINAE will not work, since the second
+  template will never be valid.
+
+  template <typename, typename = void>
+  struct isDefaultConstructableT : std::false_type {};
+  template <typename T>
+  struct isDefaultConstructableT<T, decltype(T())> : std::true_type {};
+
+*/
 
 // Test classes
 struct NoConstructorClass {
@@ -24,7 +35,7 @@ int main(int argc, char *argv[]) {
 
   if constexpr (std::is_convertible<isDefaultConstructableT<NoConstructorClass>,
                                     std::true_type>::value) {
-    std::cout << "NoConstructorClass default constructable" << std::endl;
+    std::cout << "NoConstructorClass is default constructable" << std::endl;
   } else {
     std::cout << "NoConstructorClass is not default constructable" << std::endl;
   }
