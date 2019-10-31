d31959874d0db1ae64b6a516856a68b4c81e8bb5
Maverobot <quzhengrobot@gmail.com>
2019-10-31 21:09:34 +0100

    added attributes example

---
+++ attributes_example/src/attributes_example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+[[nodiscard]] bool funcWithNoDiscard() { return true; }
+
+__attribute__((warn_unused_result)) bool funcWithUnusedResultWarning() {
+  return true;
+}
+
+[[noreturn]] void infiniteLoop() {
+  while (true) {
+  }
+}
+
+[[deprecated("This function is too old...")]] void oldFunc() {}
+
+int main(int /*argc*/, char * /*argv*/[]) {
+
+  funcWithNoDiscard();
+  funcWithUnusedResultWarning();
+
+  // gcc/clang does not give warning about unreachable code here
+  infiniteLoop();
+  std::cout << "No! That's impossible" << std::endl;
+
+  oldFunc();
+
+  return 0;
+}
