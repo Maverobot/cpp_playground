2aa543f8b086235fa98cb02ab6d52b9eaea2e5b7
Maverobot <quzhengrobot@gmail.com>
2019-11-03 21:27:30 +0100

    added quintic polynomial fitting with matplotlib

---
+++ ceres_solver_examples/src/quintic_polynomial.cpp
@@ -0,0 +1,89 @@
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+#include "matplotlibcpp.h"
+
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::Solve;
+using ceres::Solver;
+
+namespace plt = matplotlibcpp;
+
+const int kNumObservations = 4;
+const std::vector<double> data_x{2, 3, 4, 5};
+const std::vector<double> data_y{2, 1, 1, 1};
+
+struct QuinticPolynomialResidual {
+  QuinticPolynomialResidual(double x, double y) : x_(x), y_(y) {}
+  template <typename T>
+  bool operator()(const T *const a, const T *const b, const T *const c,
+                  const T *const d, const T *const e, const T *const f,
+                  T *residual) const {
+    residual[0] =
+        y_ - (a[0] * x_ * x_ * x_ * x_ * x_ + b[0] * x_ * x_ * x_ * x_ +
+              c[0] * x_ * x_ * x_ + d[0] * x_ * x_ + e[0] * x_ + f[0]);
+    return true;
+  }
+
+private:
+  const double x_;
+  const double y_;
+};
+
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  double a = 0.0;
+  double b = 0.0;
+  double c = 0.0;
+  double d = 0.0;
+  double e = 0.0;
+  double f = 0.0;
+  Problem problem;
+  for (int i = 0; i < kNumObservations; ++i) {
+    problem.AddResidualBlock(
+        new AutoDiffCostFunction<QuinticPolynomialResidual, 1, 1, 1, 1, 1, 1,
+                                 1>(
+            new QuinticPolynomialResidual(data_x[i], data_y[i])),
+        NULL, &a, &b, &c, &d, &e, &f);
+  }
+  Solver::Options options;
+  options.max_num_iterations = 25;
+  options.linear_solver_type = ceres::DENSE_QR;
+  options.minimizer_progress_to_stdout = true;
+  Solver::Summary summary;
+  Solve(options, &problem, &summary);
+  std::cout << summary.BriefReport() << std::endl;
+  std::cout << "Final   a: " << a << " b: " << b << " c: " << c << " d: " << d
+            << " e: " << e << " f: " << f << std::endl;
+
+  auto generator = [a, b, c, d, e, f](double x) {
+    return a * x * x * x * x * x + b * x * x * x * x + c * x * x * x +
+           d * x * x + e * x + f;
+  };
+
+  // Interpolated data
+  int n = 5000;
+  std::vector<double> x(n), y(n);
+  const double t_min = 0;
+  const double t_max = 5;
+  const double interval = (t_max - t_min) / n;
+  for (size_t i = 0; i < n; i++) {
+    x.at(i) = t_min + i * interval;
+    y.at(i) = generator(x.at(i));
+  }
+
+  // Set the size of output image to 1200x780 pixels
+  plt::figure_size(1200, 780);
+  // Plot line from given x and y data. Color is selected automatically.
+  plt::named_plot("via points", data_x, data_y, "*");
+  plt::named_plot("interpolated points", x, y);
+  // Set x-axis to interval [0,1000000]
+  plt::xlim(0, 5);
+  // Add graph title
+  plt::title("Cubic Polynomial Test");
+  plt::axis("equal");
+  plt::legend();
+  plt::show();
+  return 0;
+}
