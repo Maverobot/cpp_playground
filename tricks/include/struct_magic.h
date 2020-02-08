087e19b09b0467409cfdb7d361a14841d1cb0936
Zheng Qu <quzhengrobot@gmail.com>
2020-02-08 11:41:33 +0100

    [struct_magic] add perfect forwarding

--- tricks/include/struct_magic.h
+++ tricks/include/struct_magic.h
@@ -20,13 +20,14 @@ template <size_t idx, typename... Ts>
 using get_nth_type = std::tuple_element_t<idx, std::tuple<Ts...>>;
 
 template <size_t index, typename Func, typename... Ts>
-auto transform(Func f, Ts... input) {
+auto transform(Func f, Ts &&... input) {
   return f(boost::pfr::get<index>(std::forward<Ts>(input))...);
 }
 
 template <typename Func, size_t... indices, typename... Ts>
-auto transform(Func f, std::index_sequence<indices...>, Ts... input) {
-  return get_nth_type<0, Ts...>{transform<indices>(f, input...)...};
+auto transform(Func f, std::index_sequence<indices...>, Ts &&... input) {
+  return std::decay_t<get_nth_type<0, Ts...>>{
+      transform<indices>(f, std::forward<Ts>(input)...)...};
 }
 
 } // namespace detail
@@ -36,11 +37,12 @@ using all_same =
     detail::all_true<std::is_same<detail::get_nth_type<0, Ts...>, Ts...>>;
 
 // Execute function which transforms a "list" of tuples into one tuple
-template <typename Func, typename... Ts> auto transform(Func f, Ts... input) {
+template <typename Func, typename... Ts>
+auto transform(Func f, Ts &&... input) {
   return detail::transform(
       f,
-      std::make_index_sequence<
-          boost::pfr::tuple_size<detail::get_nth_type<0, Ts...>>::value>(),
+      std::make_index_sequence<boost::pfr::tuple_size<
+          std::decay_t<detail::get_nth_type<0, Ts...>>>::value>(),
       std::forward<Ts>(input)...);
 }
 
