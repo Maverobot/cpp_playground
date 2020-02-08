2853f4e3b6dac66dbc5df0a8fe99c0f0f659127a
Zheng Qu <quzhengrobot@gmail.com>
2020-02-08 11:26:47 +0100

    [struct_magic] add detail namespace

--- tricks/include/indices_trick_std.h
+++ tricks/include/indices_trick_std.h
@@ -23,17 +23,22 @@ std::array<int, std::tuple_size<Tuple>::value> execute_all(Op &&op,
 }
 } // namespace simple_test
 
-// Execute function on all element types of a tuple and return a tuple
+namespace detail {
+
 template <typename Op, typename Tuple, typename std::size_t... Is>
 auto f_all_dispatch(Op &&op, Tuple &&t, std::index_sequence<Is...>) {
   return std::make_tuple(
       std::forward<Op>(op)(std::get<Is>(std::forward<Tuple>(t)))...);
 }
 
-// Execute function on all element types of a tuple and return an array of int
+} // namespace detail
+
+// Execute function on all element types of a tuple and return a tuple of
+// results
 template <typename Op, typename Tuple>
 auto execute_all(Op &&op, const Tuple &t) {
-  return f_all_dispatch(
+  return detail::f_all_dispatch(
       op, t, std::make_index_sequence<std::tuple_size<Tuple>::value>{});
 }
+
 } // namespace indices_trick_std
