26aa5dfc4fa9e97b0c00baa043d125ade96a0368
Zheng Qu <quzhengrobot@gmail.com>
2020-03-09 22:36:45 +0100

    CHANGE: formatted and added magic struct apply

--- cpp_practices/tricks/include/struct_magic.h
+++ cpp_practices/tricks/include/struct_magic.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <array>   // std::array
-#include <utility> // std::index_sequence, std::make_index_sequence
+#include <array>    // std::array
+#include <utility>  // std::index_sequence, std::make_index_sequence
 
-#include <boost/pfr/precise.hpp> // magic_get
+#include <boost/pfr/precise.hpp>  // magic_get
 
 namespace struct_magic {
 
@@ -12,36 +12,52 @@ namespace detail {
 template <size_t idx, typename... Ts>
 using get_nth_type = std::tuple_element_t<idx, std::tuple<Ts...>>;
 
-template <typename... Conds> struct all_true : std::true_type {};
+template <typename... Conds>
+struct all_true : std::true_type {};
 template <typename Cond, typename... Conds>
 struct all_true<Cond, Conds...>
-    : std::conditional<Cond::value, all_true<Conds...>, std::false_type>::type {
-};
+    : std::conditional<Cond::value, all_true<Conds...>, std::false_type>::type {};
 
 template <typename... Ts>
 struct all_same : all_true<std::is_same<get_nth_type<0, Ts...>, Ts>...> {};
 
 template <size_t index, typename Func, typename... Ts>
-auto transform(Func f, Ts &&... input) {
+auto transform(Func f, Ts&&... input) {
   return f(boost::pfr::get<index>(std::forward<Ts>(input))...);
 }
 
 template <typename Func, size_t... indices, typename... Ts>
-auto transform(Func f, std::index_sequence<indices...>, Ts &&... input) {
-  return std::decay_t<get_nth_type<0, Ts...>>{
-      transform<indices>(f, std::forward<Ts>(input)...)...};
+auto transform(Func f, std::index_sequence<indices...>, Ts&&... input) {
+  return std::decay_t<get_nth_type<0, Ts...>>{transform<indices>(f, std::forward<Ts>(input)...)...};
 }
-} // namespace detail
 
-// Execute function which transforms a "list" of tuples into one tuple
-template <typename Func, typename... Ts,
+template <typename Op, typename Struct, typename std::size_t... Is>
+void apply_dispatch(Op&& op, Struct&& s, std::index_sequence<Is...>) {
+  // TODO: in c++17 ((void) foo(std::forward<Args>(args)), ...);
+  int dummy[] = {0,
+                 ((void)std::forward<Op>(op)(boost::pfr::get<Is>(std::forward<Struct>(s))), 0)...};
+}  // namespace detail
+
+}  // namespace detail
+
+// Apply a transformation to turn a "list" of structs into one struct
+template <typename Func,
+          typename... Ts,
           typename = std::enable_if_t<detail::all_same<Ts...>::value, int>>
-auto transform(Func f, Ts &&... input) {
+auto transform(Func f, Ts&&... input) {
   return detail::transform(
       f,
-      std::make_index_sequence<boost::pfr::tuple_size<
-          std::decay_t<detail::get_nth_type<0, Ts...>>>::value>(),
+      std::make_index_sequence<
+          boost::pfr::tuple_size<std::decay_t<detail::get_nth_type<0, Ts...>>>::value>(),
       std::forward<Ts>(input)...);
 }
 
-} // namespace struct_magic
+// Apply a transformation function on all elements of a struct and return a tuple of results
+template <typename Op, typename Struct>
+void apply(Op&& op, Struct&& s) {
+  detail::apply_dispatch(
+      op, std::forward<Struct>(s),
+      std::make_index_sequence<boost::pfr::tuple_size<std::decay_t<Struct>>::value>{});
+}
+
+}  // namespace struct_magic
--- cpp_practices/tricks/src/struct_magic_example.cpp
+++ cpp_practices/tricks/src/struct_magic_example.cpp
@@ -6,50 +6,48 @@
 #include <iterator>
 #include <string>
 
-#include <Eigen/Eigen>
-
-struct get_type_size {
-  template <typename T> int operator()(T &&) const { return sizeof(T); }
-};
-
 struct Data {
   double speed;
-  double velocity;
+  float velocity;
   double acceleration;
   int some_integer;
-
-  Eigen::Matrix<double, 1, 3> vec{1,0,1};
-
-  void foo() {};
 };
 
 template <typename T, size_t n>
-std::ostream &operator<<(std::ostream &o, const std::array<T, n> &arr) {
+std::ostream& operator<<(std::ostream& o, const std::array<T, n>& arr) {
   std::copy(arr.cbegin(), arr.cend(), std::ostream_iterator<T>(o, " "));
   return o;
 }
 
 int main() {
-  using namespace boost::pfr::ops;
+  // Util functions test
+  std::cout << struct_magic::detail::all_same<int, double, double>::value << std::endl;
+  std::cout << struct_magic::detail::all_same<int, int, int>::value << std::endl;
 
+  // Structs
   Data v1{100., 11., 21., 3};
   Data v2{200., 12., 22., 5};
-
-  std::cout << struct_magic::detail::all_same<int, double, double>::value
-            << std::endl;
-  std::cout << struct_magic::detail::all_same<int, int, int>::value
-            << std::endl;
-
+  using namespace boost::pfr::ops;
   std::cout << "v1: " << v1 << std::endl;
   std::cout << "v2: " << v2 << std::endl;
 
-  auto v1_double =
-      struct_magic::transform([](const auto &v) { return v * 2; }, v1);
+  // Transform each field of one struct with a lambda function
+  auto v1_double = struct_magic::transform([](const auto& v) { return v * 2; }, v1);
   std::cout << "v1 * 2: " << v1_double << std::endl;
 
-  auto v3 = struct_magic::transform(
-      [](const auto &a, const auto &b) { return a + b; }, v1, v2);
+  // Use corresponding fields from two structs to create a new struct with a lambda function
+  auto v3 = struct_magic::transform([](const auto& a, const auto& b) { return a + b; }, v1, v2);
   std::cout << "v3 = v1 + v2: " << v3 << std::endl;
+
+  // Transform the struct with a lambda function
+  std::cout << "v1 before: " << v1 << std::endl;
+  struct_magic::apply(
+      [i = 0](auto& item) mutable {
+        item = i;
+        i++;
+      },
+      v1);
+  std::cout << "v1 after: " << v1 << std::endl;
 
   // std::cout << res << std::endl;
 }
