2d0faf9084c8c30fc8e0dc47f062635bb4aaca89
Zheng Qu <quzhengrobot@gmail.com>
2020-03-09 22:50:36 +0100

    CHANGE: better print

--- cpp_practices/tricks/src/struct_magic_example.cpp
+++ cpp_practices/tricks/src/struct_magic_example.cpp
@@ -20,24 +20,36 @@ std::ostream& operator<<(std::ostream& o, const std::array<T, n>& arr) {
 }
 
 int main() {
+  std::cout << "=====================\n";
   // Util functions test
-  std::cout << struct_magic::detail::all_same<int, double, double>::value << std::endl;
-  std::cout << struct_magic::detail::all_same<int, int, int>::value << std::endl;
+  std::cout << "int double double are same types: "
+            << struct_magic::detail::all_same<int, double, double>::value << std::endl;
+  std::cout << "int int int are same types: "
+            << struct_magic::detail::all_same<int, int, int>::value << std::endl;
 
+  std::cout << "=====================\n";
   // Structs
   Data v1{100., 11., 21., 3};
   Data v2{200., 12., 22., 5};
   using namespace boost::pfr::ops;
   std::cout << "v1: " << v1 << std::endl;
   std::cout << "v2: " << v2 << std::endl;
+  std::cout << "=====================\n";
 
   // Transform each field of one struct with a lambda function
   auto v1_double = struct_magic::transform([](const auto& v) { return v * 2; }, v1);
   std::cout << "v1 * 2: " << v1_double << std::endl;
 
+  std::cout << "=====================\n";
   // Use corresponding fields from two structs to create a new struct with a lambda function
-  auto v3 = struct_magic::transform([](const auto& a, const auto& b) { return a + b; }, v1, v2);
+  auto v3 = struct_magic::transform(
+      [i = 0](auto& a, auto& b) mutable {
+        std::cout << "index= " << i++ << "\n";
+        return a + b;
+      },
+      v1, v2);
   std::cout << "v3 = v1 + v2: " << v3 << std::endl;
+  std::cout << "=====================\n";
 
   // Transform the struct with a lambda function
   std::cout << "v1 before: " << v1 << std::endl;
@@ -48,6 +60,7 @@ int main() {
       },
       v1);
   std::cout << "v1 after: " << v1 << std::endl;
+  std::cout << "=====================\n";
 
   // std::cout << res << std::endl;
 }
