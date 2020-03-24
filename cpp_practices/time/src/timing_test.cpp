b0ad6b69a3266ecf8468c69e342dffc9b69f069c
Zheng Qu <quzhengrobot@gmail.com>
2020-03-24 13:59:47 +0100

    CHANGE: little polish

--- cpp_practices/time/src/timing_test.cpp
+++ cpp_practices/time/src/timing_test.cpp
@@ -11,18 +11,18 @@ void setThreadPriority(int priority) {
   struct sched_param param;
   param.sched_priority = priority;
   if (sched_setscheduler(pid, SCHED_FIFO | SCHED_RESET_ON_FORK, &param) == -1) {
-    std::cerr << "failed to set thread priority";
+    throw std::runtime_error("failed to set thread priority");
   }
 }
 
 TEST_CASE("timing") {
-  timespec deadline;
-  deadline.tv_sec = 0;
-  deadline.tv_nsec = 1000000;
-
   setThreadPriority(99);
 
+  timespec deadline;
+  deadline.tv_sec = 0;
+  deadline.tv_nsec = 1000000;  // 1ms
+
   using namespace std::chrono_literals;
-  BENCHMARK("clock_gettime") { clock_nanosleep(CLOCK_REALTIME, 0, &deadline, NULL); };
-  BENCHMARK("this_thread::sleep_for") { std::this_thread::sleep_for(1ms); };
+  BENCHMARK("clock_gettime") { return clock_nanosleep(CLOCK_REALTIME, 0, &deadline, NULL); };
+  BENCHMARK("this_thread::sleep_for") { return std::this_thread::sleep_for(1ms); };
 }
