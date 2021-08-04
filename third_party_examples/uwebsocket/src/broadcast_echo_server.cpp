9063c0763df3427d435d395f313cdccf5eb5c427
Zheng Qu <quzhengrobot@gmail.com>
2021-08-04 21:57:11 +0200

    feat(uwebsocket): add uwebsocket examples (cmake support)

---
+++ third_party_examples/uwebsocket/src/broadcast_echo_server.cpp
@@ -0,0 +1,69 @@
+#include "App.h"
+
+struct us_listen_socket_t* global_listen_socket;
+
+std::string trim(std::string_view s) {
+  auto start = s.begin();
+  while (start != s.end() && std::isspace(*start)) {
+    start++;
+  }
+
+  auto end = s.end();
+  do {
+    end--;
+  } while (std::distance(start, end) > 0 && std::isspace(*end));
+
+  return std::string(start, end + 1);
+}
+
+int main() {
+  /* ws->getUserData returns one of these */
+  struct PerSocketData {};
+
+  /* Very simple WebSocket broadcasting echo server */
+  uWS::App()
+      .ws<PerSocketData>("/*", {/* Settings */
+                                .compression = uWS::DEDICATED_COMPRESSOR_3KB,
+                                .maxPayloadLength = 16 * 1024 * 1024,
+                                .idleTimeout = 120,
+                                .maxBackpressure = 1 * 1024 * 1024,
+                                /* Handlers */
+                                .upgrade = nullptr,
+                                .open =
+                                    [](auto* ws) {
+                                      /* Let's make every connection subscribe to the "broadcast"
+                                       * topic */
+                                      ws->subscribe("broadcast");
+                                    },
+                                .message =
+                                    [](auto* ws, std::string_view message, uWS::OpCode opCode) {
+                                      /* Exit gracefully if we get a closedown message (ASAN debug)
+                                       */
+                                      if (trim(message) == "closedown") {
+                                        /* Bye bye */
+                                        us_listen_socket_close(0, global_listen_socket);
+                                        ws->close();
+                                      }
+
+                                      /* Simply broadcast every single message we get */
+                                      ws->publish("broadcast", message, opCode, true);
+                                    },
+                                .drain =
+                                    [](auto* /*ws*/) {
+                                      /* Check getBufferedAmount here */
+                                    },
+                                .ping = [](auto* /*ws*/, std::string_view) {},
+                                .pong = [](auto* /*ws*/, std::string_view) {},
+                                .close =
+                                    [](auto* /*ws*/, int /*code*/, std::string_view /*message*/) {
+                                      /* We automatically unsubscribe from any topic here */
+                                    }})
+      .listen(9001,
+              [](auto* listen_socket) {
+                global_listen_socket = listen_socket;
+                if (listen_socket) {
+                  std::cout << "Listening on port " << 9001 << std::endl;
+                }
+              })
+      .run();
+}
---
+++ third_party_examples/uwebsocket/src/echo_server.cpp
@@ -0,0 +1,57 @@
+/* We simply call the root header file "App.h", giving you uWS::App and uWS::SSLApp */
+#include "App.h"
+
+/* This is a simple WebSocket echo server example.
+ * You may compile it with "WITH_OPENSSL=1 make" or with "make" */
+
+int main() {
+  /* ws->getUserData returns one of these */
+  struct PerSocketData {
+    /* Fill with user data */
+  };
+
+  /* Keep in mind that uWS::SSLApp({options}) is the same as uWS::App() when compiled without SSL
+   * support. You may swap to using uWS:App() if you don't need SSL */
+  uWS::App()
+      .ws<PerSocketData>("/*",
+                         {/* Settings */
+                          .compression = uWS::SHARED_COMPRESSOR,
+                          .maxPayloadLength = 16 * 1024 * 1024,
+                          .idleTimeout = 16,
+                          .maxBackpressure = 1 * 1024 * 1024,
+                          .closeOnBackpressureLimit = false,
+                          .resetIdleTimeoutOnSend = false,
+                          .sendPingsAutomatically = true,
+                          /* Handlers */
+                          .upgrade = nullptr,
+                          .open =
+                              [](auto* /*ws*/) {
+                                /* Open event here, you may access ws->getUserData() which
+                                 * points to a PerSocketData struct */
+                              },
+                          .message = [](auto* ws, std::string_view message,
+                                        uWS::OpCode opCode) { ws->send(message, opCode, true); },
+                          .drain =
+                              [](auto* /*ws*/) {
+                                /* Check ws->getBufferedAmount() here */
+                              },
+                          .ping =
+                              [](auto* /*ws*/, std::string_view) {
+                                /* Not implemented yet */
+                              },
+                          .pong =
+                              [](auto* /*ws*/, std::string_view) {
+                                /* Not implemented yet */
+                              },
+                          .close =
+                              [](auto* /*ws*/, int /*code*/, std::string_view /*message*/) {
+                                /* You may access ws->getUserData() here */
+                              }})
+      .listen(9001,
+              [](auto* listen_socket) {
+                if (listen_socket) {
+                  std::cout << "Listening on port " << 9001 << std::endl;
+                }
+              })
+      .run();
+}
