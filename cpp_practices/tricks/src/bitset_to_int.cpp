f933decd54f1214134e3337b03a0fb7f4846e163
Zheng Qu <quzhengrobot@gmail.com>
2024-07-20 23:58:47 +0200

    fix: Fix compilation warnings / errors

--- cpp_practices/tricks/src/bitset_to_int.cpp
+++ cpp_practices/tricks/src/bitset_to_int.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  std::bitset<16> bits{static_cast<ulong>(-58)};
+  std::bitset<16> bits{static_cast<unsigned long>(-58)};
 
   std::cout << "int16_t value: " << static_cast<int16_t>(bits.to_ulong());
   return 0;
